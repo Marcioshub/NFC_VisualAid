49faedaf353c22a522a40f79eb5fbd61b1e10628
Marcio Castillo <mecastillo00@gmail.com>
2016-04-14 12:52:31 -0400

    adding NFC code

--- app/src/main/java/com/example/mars/nfc_visualaid/MainActivity.java
+++ app/src/main/java/com/example/mars/nfc_visualaid/MainActivity.java
@@ -1,13 +1,121 @@
 package com.example.mars.nfc_visualaid;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    NfcAdapter nfcAdapter;
+    TextView tagText;
+    TextToSpeech ttSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //added code by me
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        //tb = (ToggleButton)findViewById(R.id.toggleButton);
+        tagText = (TextView) findViewById(R.id.textView);
+
+        //check if NFC is enabled
+        if(!nfcAdapter.isEnabled())
+            Toast.makeText(this, "Please enable NFC", Toast.LENGTH_SHORT).show();
+
+        ttSpeech =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    ttSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        ttSpeech.setSpeechRate((float) .6);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        disableForegroundDispatchSystem();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        enableForegroundDispatchSystem();
+    }
+
+    private void enableForegroundDispatchSystem(){
+        Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[] {};
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+    }
+
+    private void disableForegroundDispatchSystem(){
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+
+        if(intent.hasExtra(NfcAdapter.EXTRA_TAG)){
+            Toast.makeText(this, "NFC intent", Toast.LENGTH_SHORT).show();
+
+            Parcelable[] parcelable = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            if(parcelable != null && parcelable.length > 0){
+                readTextFromMessage((NdefMessage) parcelable[0]);
+            }
+            else{
+                Toast.makeText(this, "No ndef message found!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void readTextFromMessage(NdefMessage msg) {
+        NdefRecord[] ndefRecords = msg.getRecords();
+
+        if(ndefRecords != null && ndefRecords.length > 0){
+            NdefRecord ndefRecord = ndefRecords[0];
+            String tagContent = getTextFromNdefRecord(ndefRecord);
+            tagText.setText(tagContent);
+            ttSpeech.speak(tagContent, TextToSpeech.QUEUE_FLUSH, null);
+        }
+        else{
+            Toast.makeText(this, "No ndef records found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public String getTextFromNdefRecord(NdefRecord ndefRecord){
+        String tagContent = null;
+        try{
+            byte[] payload = ndefRecord.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize + 1, payload.length - languageSize - 1, textEncoding);
+        }catch(Exception e){
+            Log.e("getTextFromNdefRecord", e.getMessage(), e);
+        }
+        return tagContent;
     }
 }
